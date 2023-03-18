f77d93e54325fc78dbcfb4893221474a5f8b8ba1
mistertandon <enggparveshtandon@gmail.com>
2023-03-18 15:27:09 +0530

    feature: Add tailwind based basic layout

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  corePlugins: {
+    // preflight: false,
+  },
+  content: ["./index.html", "./src/**/*.{html, js}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xxl': '1536px',
+      'xxxl': '1920px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
